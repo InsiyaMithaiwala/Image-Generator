b3c147cc82e7ac45da863fbe5d42433b28fdcc63
Swayam Pandya <swayampandya1236@gmail.com>
2023-07-15 17:21:45 +0530

    post page added

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ const App = () => (
   <BrowserRouter>
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
       <Link to="/">
-        <img src={logo} alt="logo" className="w-28 object-contain" />
+        {/* <img src={logo} alt="logo" className="w-28 object-contain" /> */}
+        <h1 style={{fontFamily:'cursive',fontSize:'30px'}}>* Mind-Scape</h1>
       </Link>
 
       <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md">Create</Link>
